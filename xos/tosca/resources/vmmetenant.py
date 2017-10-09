10b6256eec8bca13ca538b8b526f8ea0f6bbab7d
Sapan Bhatia <gwsapan@gmail.com>
2017-10-09 18:05:35 -0400

    CORD-2050: Automatically bring up VMs for service instances
    Change-Id: I8cf7067fa1319c210c7ab638aa788fc179247933
    (cherry picked from commit 81c8809c471995dbf9616d2753d8b5cd2f110413)

--- xos/tosca/resources/vmmetenant.py
+++ xos/tosca/resources/vmmetenant.py
@@ -27,15 +27,15 @@ class XOSVMMETenant(XOSResource):
 
         provider_name = self.get_requirement("tosca.relationships.TenantOfService", throw_exception=throw_exception)
         if provider_name:
-            args["provider_service"] = self.get_xos_object(Service, throw_exception=throw_exception, name=provider_name)
+            args["owner"] = self.get_xos_object(Service, throw_exception=throw_exception, name=provider_name)
 
         return args
 
     def get_existing_objs(self):
         args = self.get_xos_args(throw_exception=False)
-        provider_service = args.get("provider", None)
-        if provider_service:
-            return [ self.get_xos_object(provider_service=provider_service) ]
+        owner = args.get("provider", None)
+        if owner:
+            return [ self.get_xos_object(owner=owner) ]
         return []
 
     def postprocess(self, obj):
