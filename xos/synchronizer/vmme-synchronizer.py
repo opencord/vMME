a329945dcf5007402e024b6eeb708934c40ad191
Matteo Scandolo <teo@opennetworking.org>
2018-06-12 13:42:56 -0700

    [CORD-3022] Splitting config and removing unused files
    Change-Id: Ie5582e253191183f2500d4e0a5fa5c44fa233d2f

--- xos/synchronizer/vmme-synchronizer.py
+++ xos/synchronizer/vmme-synchronizer.py
@@ -19,11 +19,16 @@
 import importlib
 import os
 import sys
-
 from xosconfig import Config
-config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/vmme_config.yaml')
 
-Config.init(config_file, 'synchronizer-config-schema.yaml')
+base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
+
+if os.path.isfile(mounted_config_file):
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
+else:
+    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
+
 observer_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../synchronizers/new_base")
 
 sys.path.append(observer_path)
